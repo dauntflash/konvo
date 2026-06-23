c89c592696508ede18a7556e6c94b941deae1ecf
Dauntflash <ndwiga14053@student.mmarau.ac.ke>
2026-06-23 13:47:11 +0300

    enhance next.config.ts to support optional eslint configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 
-const nextConfig: NextConfig = {
+const nextConfig: NextConfig & {
+  eslint?: {
+    ignoreDuringBuilds: boolean;
+  };
+} = {
   eslint: {
     ignoreDuringBuilds: true,
   },
