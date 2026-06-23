a95ac7c7e287cc7eb32ca56c2601c2ce91148ccb
Dauntflash <ndwiga14053@student.mmarau.ac.ke>
2026-06-23 13:59:32 +0300

    update PocketBase initialization to use environment variable for URL

--- lib/pocketbase.ts
+++ lib/pocketbase.ts
@@ -1,7 +1,7 @@
 // lib/pocketbase.js
 import PocketBase from "pocketbase";
 
-const pb = new PocketBase("http://127.0.0.1:8090");
+const pb = new PocketBase(process.env.NEXT_PUBLIC_PB_URL || "http://127.0.0.1:8090");
 pb.autoCancellation(false);
 
 export default pb;
